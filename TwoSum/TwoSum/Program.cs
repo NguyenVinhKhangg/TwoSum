fb17d96ad472185c40b4a2aae309e009d1d467c1
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-11-28 21:59:58 +0700

    LeetCode: TwoSum v1.0

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSum twoSum = new TwoSum();
+
+            int[] arr = { 1, 2, 3 };
+            int target = 3;
+
+            int[] result = twoSum.CaculateTwoSum(arr, target);
+            Console.WriteLine("[" + string.Join(", ",result) + "]");
+        }
+    }
+}
---
+++ TwoSum/TwoSum/TwoSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class TwoSum 
+    {
+        public int[] CaculateTwoSum(int[] nums, int target)
+        {
+            int[] arr = new int[2];
+            HashSet<int> compareSet = new HashSet<int>();
+
+            int i;
+            for (i = 0; i < nums.Length; i++)
+            {
+                if (compareSet.Contains(target - nums[i]))
+                {
+                    break;
+                }
+                compareSet.Add(nums[i]);
+            }
+
+            for (int j = 0; j < i; j++)
+            {
+                if (target - nums[i] == nums[j])
+                {
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+
+            }
+            return arr;
+        }
+    }
+}
